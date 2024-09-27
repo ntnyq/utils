aaeba06d07c6629c3ae7398ab874c00e81fbe39e
ntnyq <ntnyq13@gmail.com>
2024-09-27 12:16:18 +0800

    fix: fix type of `toArray`

--- src/array/toArray.ts
+++ src/array/toArray.ts
@@ -1,6 +1,11 @@
 import type { Arrayable, Nullable } from '../types'
 
-export function toArray<T>(val?: Nullable<Arrayable<T>>): Arrayable<T> {
+/**
+ * Converts a value to an array.
+ * @param val - The value to convert.
+ * @returns The array.
+ */
+export function toArray<T>(val?: Nullable<Arrayable<T>>): T[] {
   val = val ?? []
   return Array.isArray(val) ? val : [val]
 }

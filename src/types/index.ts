a5d9a26ef955ad064658773b09c58a696c8016fe
ntnyq <ntnyq13@gmail.com>
2025-02-18 22:18:02 +0800

    feat(types): add type `LiteralUnion` and `NonEmptyObject`

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,7 @@ export type Awaitable<T> = Promise<T> | T
 export type Callable<T> = AnyFn<any, T> | T
 
 export type MayBe<T> = T | undefined
+
 export type Nullable<T> = T | null
 
 /**
@@ -33,4 +34,5 @@ export type PrimitiveType =
 export type ResolvedOptions<T> = T extends boolean ? never : NonNullable<T>
 
 export * from './json'
+export * from './utils'
 export * from './module'
---
+++ src/types/utils.ts
@@ -0,0 +1,13 @@
+/**
+ * A literal type that supports custom further strings but preserves autocompletion in IDEs.
+ *
+ * @see https://github.com/microsoft/TypeScript/issues/29729#issuecomment-471566609
+ */
+export type LiteralUnion<Union extends Base, Base = string> =
+  | Union
+  | (Base & { zz_IGNORE_ME?: never })
+
+/**
+ * Non empty object `{}`
+ */
+export type NonEmptyObject<T> = T extends Record<string, never> ? never : T

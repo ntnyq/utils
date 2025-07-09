3eea1715c019e3c8be4662bb094ade67d27a32ca
ntnyq <ntnyq13@gmail.com>
2025-07-09 11:46:30 +0800

    fix(time): reduce side effects

--- src/misc/time.ts
+++ src/misc/time.ts
@@ -3,28 +3,47 @@
  * @module Time
  */
 
-export const ONE_SECOND_MILLSECONDS: number = 1000
-export const ONE_MINUTE_MILLSECONDS: number = 60 * 1000
-export const ONE_HOUR_MILLSECONDS: number = 60 * 60 * 1000
-export const ONE_DAY_MILLSECONDS: number = 24 * 60 * 60 * 1000
-export const ONE_WEEK_MILLSECONDS: number = 7 * 24 * 60 * 60 * 1000
-
+/**
+ * Converts seconds to milliseconds.
+ * @param count - The number of seconds.
+ * @returns The equivalent number of milliseconds.
+ */
 export function seconds(count: number): number {
-  return count * ONE_SECOND_MILLSECONDS
+  return count * 1000
 }
 
+/**
+ * Converts minutes to milliseconds.
+ * @param count - The number of minutes.
+ * @returns The equivalent number of milliseconds.
+ */
 export function minutes(count: number): number {
-  return count * ONE_MINUTE_MILLSECONDS
+  return count * 60 * 1000
 }
 
+/**
+ * Converts hours to milliseconds.
+ * @param count - The number of hours.
+ * @returns The equivalent number of milliseconds.
+ */
 export function hours(count: number): number {
-  return count * ONE_HOUR_MILLSECONDS
+  return count * 60 * 60 * 1000
 }
 
+/**
+ * Converts days to milliseconds.
+ * @param count - The number of days.
+ * @returns The equivalent number of milliseconds.
+ */
 export function days(count: number): number {
-  return count * ONE_DAY_MILLSECONDS
+  return count * 24 * 60 * 60 * 1000
 }
 
+/**
+ * Converts weeks to milliseconds.
+ * @param count - The number of weeks.
+ * @returns The equivalent number of milliseconds.
+ */
 export function weeks(count: number): number {
-  return count * ONE_WEEK_MILLSECONDS
+  return count * 7 * 24 * 60 * 60 * 1000
 }

451abe76423c42cb9bfabbab7b0b93920074edb3
ntnyq <ntnyq13@gmail.com>
2025-12-25 15:32:05 +0800

    feat: add more special chars

--- src/constants/char.ts
+++ src/constants/char.ts
@@ -2,5 +2,28 @@
  * Special chars
  */
 export const SPECIAL_CHAR = {
+  /**
+   * 中文顿号
+   */
+  chineseComma: '、',
+  /**
+   * 英文逗号
+   */
+  englishComma: ',',
+  /**
+   * 英文句号
+   */
+  englishPeriod: '.',
+  /**
+   * 连接符
+   */
+  hyphen: '-',
+  /**
+   * 换行
+   */
   newline: '\n',
+  /**
+   * 空格
+   */
+  whitespace: ' ',
 }
